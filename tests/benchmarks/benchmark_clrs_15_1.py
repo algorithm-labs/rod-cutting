9fdf6b190caa87f2e25bfdd7e904a823b0ef2b0f
Sebastian Winkler <sebwink@pm.me>
2023-02-19 18:36:03 +0000

    Fix benchmark name

--- tests/benchmarks/benchmark_clrs_15_1.py
+++ tests/benchmarks/benchmark_clrs_15_1.py
@@ -19,6 +19,6 @@ BENCHMARKS = itertools.product(ROD_SIZES, SOLVERS)
 
 
 @pytest.mark.parametrize("rod_size, Solver", BENCHMARKS)
-def benchmark_bottom_up_clrs_15_1(benchmark, rod_size, Solver):
+def benchmark_clrs_15_1(benchmark, rod_size, Solver):
     instance = CLRS_15_1.restrict(rod_size)
     benchmark(Solver.solve, instance)
